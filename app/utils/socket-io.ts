bf7649c9644486552301f2da81da630328cd4d0f
Alexis Rojas <rojas.estrada19@gmail.com>
2022-10-23 23:38:54 -0600

    Fix CORS

--- app/utils/socket-io.ts
+++ app/utils/socket-io.ts
@@ -17,7 +17,7 @@ class WS {
     if (!instance) {
       instance = new IO(this.server, {
         cors: {
-          origin: 'http://localhost:3000',
+          origin: process.env.FRONTEND_URL || 'http://localhost:3000',
           methods: ['GET', 'POST'],
         },
         // transports: ['websocket', 'polling'],
